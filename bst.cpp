0f715130606963fcfcc8393de5a9187ba483a2d6
Emman-pip <parrenoemmanuel756@gmail.com>
2024-05-10 14:25:05 +0800

    Add insert and start the delete.

--- bst.cpp
+++ bst.cpp
@@ -37,25 +37,33 @@ private:
     return root;
   }
 
+  // optimize this. Make one function.
+  bool insertBack(int value, Node *node) {
+    if (node == NULL || node->data == value) {
+      return 0;
+    } else if (node->data > value && node->left == NULL) {
+      Node *newnode = new Node(value);
+      node->left = newnode;
+      return 1;
+    } else if (node->data < value && node->right == NULL) {
+      Node *newnode = new Node(value);
+      node->right = newnode;
+      return 1;
+    } else if (node->data > value) {
+      return insertBack(value, node->left);
+    } else if (node->data < value) {
+      return insertBack(value, node->right);
+    }
+    return 1;
+  };
+
 public:
   Node *root;
-  Tree(vector<int> arr) { this->root = buildTree(arr); }
+  Tree(vector<int> arr) { this->root = buildTree(arr); };
+  bool insert(int value) { return this->insertBack(value, root); };
+  // create the delete node functionality
+  bool deleteNode(int value) { return false; };
 };
-
-// const prettyPrint = (node, prefix = "", isLeft = true) => {
-// 	if (node == null) {
-// 		return;
-// 	}
-// 	if (node.right != null) {
-// 		prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`,
-// false);
-// 	}
-// 	console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-// 	if (node.left != null) {
-// 		prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`,
-// true);
-// 	}
-// };
 
 void prettyPrint(Node *node, string prefix = "", bool isLeft = true) {
   if (!node) {
@@ -70,8 +78,12 @@ void prettyPrint(Node *node, string prefix = "", bool isLeft = true) {
   }
 }
 int main() {
-  vector<int> arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  vector<int> arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3289};
   Tree *tree = new Tree(arr);
+  tree->insert(20);
+  tree->insert(30);
+  tree->insert(10);
+  tree->insert(-80);
   prettyPrint(tree->root);
 
   return 0;
