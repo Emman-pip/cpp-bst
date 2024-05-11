53a17e253c501d20f7a1285ec0cb784146dd2f4e
Emman-pip <parrenoemmanuel756@gmail.com>
2024-05-11 13:10:33 +0800

    Add partial delete funcitonalities.

--- bst.cpp
+++ bst.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <iostream>
+#include <ostream>
 #include <vector>
 
 using namespace std;
@@ -38,23 +39,16 @@ private:
   }
 
   // optimize this. Make one function.
-  bool insertBack(int value, Node *node) {
-    if (node == NULL || node->data == value) {
-      return 0;
-    } else if (node->data > value && node->left == NULL) {
+  Node *insertBack(int value, Node *node) {
+    if (node == NULL) {
       Node *newnode = new Node(value);
-      node->left = newnode;
-      return 1;
-    } else if (node->data < value && node->right == NULL) {
-      Node *newnode = new Node(value);
-      node->right = newnode;
-      return 1;
-    } else if (node->data > value) {
-      return insertBack(value, node->left);
-    } else if (node->data < value) {
-      return insertBack(value, node->right);
+      return newnode;
+    } else if (value < node->data) {
+      node->left = insertBack(value, node->left);
+    } else if (value > node->data) {
+      node->right = insertBack(value, node->right);
     }
-    return 1;
+    return node;
   };
 
 public:
@@ -62,7 +56,42 @@ public:
   Tree(vector<int> arr) { this->root = buildTree(arr); };
   bool insert(int value) { return this->insertBack(value, root); };
   // create the delete node functionality
-  bool deleteNode(int value) { return false; };
+  bool deleteNode(int value) {
+    // save the prev, direction, and the current node
+    Node *prev = NULL;
+    Node *current = root;
+    bool isLeft;
+    // loop to find the node
+    while (current->data != value) {
+      prev = current;
+      if (current->data > value) {
+        current = current->left;
+        isLeft = true;
+        continue;
+      }
+      current = current->right;
+      isLeft = false;
+    }
+    if (!current)
+      return false;
+
+    // do with multiple chilren
+    if (current->left && current->right) {
+      cout << "Di pa nagagawa toh. " << endl;
+    }
+    // do with one child
+    else if (current->left) {
+      (isLeft) ? prev->left = current->left : prev->right = current->left;
+
+    } else if (current->right) {
+      (isLeft) ? prev->left = current->right : prev->right = current->right;
+    }
+    // do with no child -- DONE
+    else {
+      (isLeft) ? prev->left = NULL : prev->right = NULL;
+    }
+    return true;
+  };
 };
 
 void prettyPrint(Node *node, string prefix = "", bool isLeft = true) {
@@ -81,9 +110,14 @@ int main() {
   vector<int> arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3289};
   Tree *tree = new Tree(arr);
   tree->insert(20);
+  tree->insert(10);
+  tree->insert(0);
   tree->insert(30);
   tree->insert(10);
   tree->insert(-80);
+  cout << tree->deleteNode(4) << endl;
+  cout << tree->deleteNode(10) << endl;
+  cout << tree->deleteNode(3289) << endl;
   prettyPrint(tree->root);
 
   return 0;
